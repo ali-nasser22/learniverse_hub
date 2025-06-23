0e08a8f8ec9f31cbfdc6cccf1bc738b62af2a01c
Ali Nasser <alinasser.t22@gmail.com>
2025-06-23 19:49:19 +0300

    changing middleware.js location

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { authConfig } from "../auth.config";
 import { NextResponse } from "next/server";
-import { PUBLIC_ROUTES, LOGIN, ROOT } from "./src/lib/routes";
+import { PUBLIC_ROUTES, LOGIN, ROOT } from "./lib/routes";
 
 const { auth } = NextAuth(authConfig);
 
